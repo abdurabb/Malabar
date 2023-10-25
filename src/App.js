de879c735c78f5368dbab4647f1aea1aa262fb3e
‘abdurabb’ <abdurabb00@gmail.com>
2023-10-25 11:00:36 +0530

    page location changed"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 // import LandPage from '../../malabar/src/Pages/LandPage'
-import LandPage from '../../malabar/src/Pages/LandPage';
+import LandPage from  '../src/Pages/LandPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
